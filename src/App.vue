37d9e490d3a78c1c94fcbed43a9a1c2ed3af6294
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-03 11:21:23 +0200

    add colors palette

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script></script>
 
-<template>
-  <h1 class="text-center">Sono tutt'uno con la forza, la forza è con me</h1>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>

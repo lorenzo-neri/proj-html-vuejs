8a3ac67a9a7952d3250a534672720fa4ed80748b
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-03 13:48:29 +0200

    add header structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,99 @@
-<script></script>
+<script>
+export default {
 
-<template></template>
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+
+}
+</script>
+
+<template>
+  <div id="jumbotron" class="container-fluid text-white">
+
+
+
+    <header>
+      <div class="container mw_1180 py-3 pb-5">
+        <div class="row justify-content-between">
+          <div class="col">
+            logo
+          </div>
+          <div class="col">
+            menù
+          </div>
+          <div class="col">
+            icon
+          </div>
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </header>
+
+
+
+    <div class="container py-5">
+      <div class="row py-5 justify-content-start">
+
+        <div class="col-1">
+          <div>icon</div>
+          <div>icon</div>
+          <div>icon</div>
+          <div>icon</div>
+        </div>
+        <!-- /LEFT ICON -->
+
+        <div class="col-10 text-center">
+          <div class="text-uppercase">
+            Welcome to RaxG
+          </div>
+          <!-- /WELCOME -->
+
+          <h1 class="text-uppercase">
+            Are you ready for your next challenge?
+          </h1>
+          <!-- /h1 -->
+
+          <div class="d-flex justify-content-center">
+            <a class="btn es_button d-flex align-items-center" href="##">
+              Read More
+              <svg style="width: 1rem;" class="svg-inline--fa fa-right-long ps-1" aria-hidden="true" focusable="false"
+                data-prefix="fas" data-icon="right-long" role="img" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512" data-fa-i2svg="">
+                <path fill="currentColor"
+                  d="M504.3 273.6l-112.1 104c-6.992 6.484-17.18 8.218-25.94 4.406c-8.758-3.812-14.42-12.45-14.42-21.1L351.9 288H32C14.33 288 .0002 273.7 .0002 255.1S14.33 224 32 224h319.9l0-72c0-9.547 5.66-18.19 14.42-22c8.754-3.809 18.95-2.075 25.94 4.41l112.1 104C514.6 247.9 514.6 264.1 504.3 273.6z">
+                </path>
+              </svg>
+            </a>
+
+            <a href="##" class="btn es_button d-flex align-items-center rounded-circle">
+              <svg class="svg-inline--fa fa-play" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play"
+                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg="">
+                <path fill="currentColor"
+                  d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z">
+                </path>
+              </svg>
+            </a>
+          </div>
+          <!-- /BUTTONS -->
+
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+
+
+  </div>
+  <!-- /#jumbotron /.container-fluid --></template>
 
 <style lang="scss" scoped></style>

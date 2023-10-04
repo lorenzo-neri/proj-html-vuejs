b949dd5ffa6e46f2643f365cedf0ec95ff353e17
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-04 11:46:40 +0200

    add AppFooter component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
 
@@ -11,6 +12,7 @@ export default {
   },
   components: {
     AppHeader,
+    AppFooter,
   }
 
 }
@@ -18,6 +20,35 @@ export default {
 
 <template>
   <AppHeader />
+
+  <!--######## TEST ########-->
+  <main class="bg-danger">
+    <div class="container p-5 position-relative">
+      <div class="row p-5">
+        <div class="col p-5 text-center">
+          MAIN
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <div id="test" class="d-flex justify-content-center text-white align-items-center">
+            form
+          </div>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+  </main>
+  <!--######## TEST ########-->
+
+
+  <AppFooter />
 </template>
 
 <style lang="scss" scoped></style>

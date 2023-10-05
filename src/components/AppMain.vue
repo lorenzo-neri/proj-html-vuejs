a364b94d1a59ba7ef02132ac06c82543e3dbb263
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-05 10:43:55 +0200

    add PlayButtonCircle component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+import PlayButtonCircle from './PlayButtonCircle.vue';
 
 export default {
 
@@ -9,7 +10,7 @@ export default {
         }
     },
     components: {
-
+        PlayButtonCircle,
     }
 
 }
@@ -67,17 +68,7 @@ export default {
                                 </svg>
                             </a>
 
-                            <a style="padding: 2rem; scale: .7;"
-                                class="btn es_button d-flex align-items-center justify-content-center rounded-circle"
-                                href="###">
-                                <svg width="1rem" class="svg-inline--fa fa-play" aria-hidden="true" focusable="false"
-                                    data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 384 512">
-                                    <path fill="currentColor"
-                                        d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z">
-                                    </path>
-                                </svg>
-                            </a>
+                            <PlayButtonCircle />
                         </div>
                         <!-- /buttons -->
 
@@ -171,7 +162,23 @@ export default {
 
         <!-- #live################# -->
 
-
+        <section id="live" class="d-flex justify content-center">
+            <div class="container p-5 mw1180">
+                <div class="row">
+                    <div class="col">
+                        video
+                    </div>
+                    <!-- /.col -->
+                    <div class="col">
+                        testo e video
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </section>
+        <!-- /#live -->
 
         <!-- /#live################# -->
 
